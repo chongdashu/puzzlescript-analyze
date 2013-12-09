61a28d584b4ed476eefd818da919d0cc9b144d93
Chong-U Lim <lim.chong.u@gmail.com>
2013-12-09 00:20:30 -0500

    Added general parsing for all sections.

--- python/puzzlescript.py
+++ python/puzzlescript.py
@@ -12,36 +12,61 @@ class Script(object):
 	def parse(self, txt):
 			
 		section = Section.create(Section.TYPE_PRELUDE)
+		self.add_section(section)
 		
 		lines = txt.split("\n")
 		for line in lines:
-			section.parse_line(line)
+			if Section.is_section(line):
+				print 'Parsing section: %s' %(line)
+				section = self.create_section(line)
+				
+			if section:
+				section.parse_line(line)
 
-		self.sections[Section.TYPE_PRELUDE] = section
+	def add_section(self, section):
+		self.sections[section.type] = section
 
 	def section(self, name):
 		return self.sections.get(name)
 
+	def create_section(self, name):
+		
+		section = Section.create(name)
+		if section:
+			self.add_section(section)
 
+		return section
 
-
+ 
 	def __repr__(self):
 		return "sections: %s" %(self.sections)
 
 
 class Section(object):
 
-	TYPE_PRELUDE = "Prelude"
-	TYPE_OBJECTS = "Objects"
-	TYPE_LEGEND = "Legend"
-	TYPE_SOUNDS = "Sounds"
-	TYPE_COLLISIONLAYERS = "CollisionLayers"
-	TYPE_RULES = "Rules"
-	TYPE_WINCONDITIONS = "WinConditions"
-	TYPE_LEVELS = "Levels"
+	TYPE_PRELUDE = "prelude"
+	TYPE_OBJECTS = "objects"
+	TYPE_LEGEND = "legend"
+	TYPE_SOUNDS = "sounds"
+	TYPE_COLLISIONLAYERS = "collisionlayers"
+	TYPE_RULES = "rules"
+	TYPE_WINCONDITIONS = "winconditions"
+	TYPE_LEVELS = "levels"
 
-	def __init__(self):
+	TYPES = [
+		TYPE_PRELUDE,
+		TYPE_OBJECTS,
+		TYPE_LEGEND,
+		TYPE_SOUNDS,
+		TYPE_COLLISIONLAYERS,
+		TYPE_RULES,
+		TYPE_WINCONDITIONS,
+		TYPE_LEVELS
+	]
+
+	def __init__(self, type_):
 		self.tokens = {}
+		self.type = type_
 
 	def set(self, key, value):
 		self.tokens[key] = value
@@ -49,15 +74,24 @@ class Section(object):
 	def get(self, key):
 		return self.tokens.get(key)
 
+	def type(self):
+		return self.type
+
 	def __repr__(self):
 		return "tokens: %s" %(self.tokens.keys())
 
 	@staticmethod
 	def create(type_):
-		if type_ == Section.TYPE_PRELUDE:
-			return PreludeSection()
+		sectionType = type_.lower()
+
+		if sectionType == Section.TYPE_PRELUDE:
+			return PreludeSection(sectionType)
 
 		return None
+
+	@staticmethod
+	def is_section(name):
+		return name.lower() in Section.TYPES
 
 
 
@@ -81,8 +115,8 @@ class PreludeSection(Section):
 	TOKEN_YOUTUBE = "youtube"
 	TOKEN_ZOOMSCREEN = "zoomscreen"
 
-	def __init__(self):
-		Section.__init__(self)
+	def __init__(self, type_):
+		Section.__init__(self, type_)
 
 		self.title = None
 		self.author = None
@@ -99,6 +133,8 @@ class PreludeSection(Section):
 			value = match.group(2)
 
 			self.set(token, value)
+
+
 
 
 
